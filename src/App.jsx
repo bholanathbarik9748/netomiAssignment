189aa8a3392d45954ca9576bff47b88e5083a19b
bholanathbarik9748 <bholanathbarik9748@gmail.com>
2023-04-27 20:34:03 +0530

    route v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import Form from './page/form'
 
@@ -7,14 +8,14 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route path='/'  element={<Home />} />
-          <Route path='/form'  element={<Form />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/form" element={<Form />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-          <iframe src="/form" width="600" height="600"></iframe>
+      <iframe src="/form" width="600" height="600" allowfullscreen></iframe>
     </div>
   )
 }
