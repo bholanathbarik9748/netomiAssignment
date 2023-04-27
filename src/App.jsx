8cacc52d1cd8cc0613d82750595d5fb87beecc3b
bholanathbarik9748 <bholanathbarik9748@gmail.com>
2023-04-27 22:12:52 +0530

    Final Commit v1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
-import Form from './page/form'
 
 function App() {
 
@@ -12,7 +11,6 @@ function App() {
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/" element={<Form />} /> */}
           </Routes>
         </Suspense>
       </Router>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <iframe src="/form" width="600" height="600" allowfullscreen></iframe>
+      <iframe src="https://lovely-sunflower-5e5c0c.netlify.app/" width="600" height="600" allowfullscreen></iframe>
     </div>
   )
 }
